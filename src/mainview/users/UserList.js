093d393fb6f95c3199c0b699704004d16090f320
Yasir Fayyaz <yasirfayyaz047@gmail.com>
2023-01-24 18:12:14 +0500

    userlist file updated

--- src/mainview/users/UserList.js
+++ src/mainview/users/UserList.js
@@ -3,15 +3,9 @@ import {useNavigate} from 'react-router-dom';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
 import CustomizedDialogs from '../../components/dialog';
-<<<<<<< HEAD
 import CustomizedDialogsEdit from '../../components/dialog_edit';
-=======
-import CustomizedDialogs_edit from '../../components/dialog_edit';
-import CustomizedDialogs_delete from '../../components/dialog_delete';
->>>>>>> 56aea5c8edd09c1ec7b4b86bb5e664fdc46c6101
 import AddUser from '../../forms/add_user/AddUser';
 import EditUser from '../../forms/EditUser';
-import DeleteUser from '../../forms/DeleteUser';
 import axios from 'axios';
 // const rows: GridRowsProp = [
 //   { id: 1, col1: 'Hello', col2: 'World' },
@@ -21,40 +15,27 @@ import axios from 'axios';
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID' },
-    { field: 'uname', headerName: 'Full Name', width: 150 },
+    { field: 'uname', headerName: 'Name', width: 150 },
     { field: 'email', headerName: 'Email', width: 150 },
-    { field: 'site', headerName: 'Site', width: 150 },
+    { field: 'password', headerName: 'password', width: 150 },
+    { field: 'site', headerName: 'site_Name', width: 150 },
     { field: 'contact', headerName: 'Contact', width: 150 },
-    { field: 'address', headerName: 'Address', width: 150 },
-    { field: 'empType', headerName: 'Empolyee Type', width: 150 },
-    { field: 'consultant', headerName: 'Consultant', width: 150 },
-    { field: 'empSec', headerName: 'Empolyee Section', width: 150 },
-    { field: 'empField', headerName: 'Empolyee Field', width: 150 },
-    { field: 'empRole', headerName: 'Empolyee Role', width: 150 },
-    // { field: 'empTeam', headerName: 'EmpolyeeTeam', width: 150 },
-    // { field: 'status', headerName: 'status', width: 150 },
+    { field: 'address', headerName: 'address', width: 150 },
+    { field: 'empType', headerName: 'Emp_Type', width: 150 },
+    { field: 'consultant', headerName: 'consultant', width: 150 },
+    { field: 'empSec', headerName: 'Emp_Sec', width: 150 },
+    { field: 'empField', headerName: 'Emp_Field', width: 150 },
+    { field: 'empRole', headerName: 'Emp_Role', width: 150 },
+    { field: 'empTeam', headerName: 'Emp_Team', width: 150 },
+    { field: 'status', headerName: 'status', width: 150 },
     { field: 'action', headerName: 'Action', width: 150, renderCell:(value) => {
       return (
-<<<<<<< HEAD
         <CustomizedDialogsEdit size='small'>
         <EditUser uname={value.row.uname} email={value.row.email} password={value.row.password}
          site={value.row.site} contact={value.row.contact} address={value.row.address} empType={value.row.empType}
          consultant={value.row.consultant} empSec={value.row.empSec} empField={value.row.empField} empRole={value.row.empRole}
          empTeam={value.row.empTeam} status={value.row.status}/>
         </CustomizedDialogsEdit>
-=======
-        <>
-        <CustomizedDialogs_edit size='small'>
-        <EditUser fname={value.row.fname} lname={value.row.lname} email={value.row.email} password={value.row.password} 
-         site={value.row.site} contact={value.row.contact} address={value.row.address} empType={value.row.empType}
-         consultant={value.row.consultant} empSec={value.row.empSec} empField={value.row.empField} empRole={value.row.empRole}
-         />
-        </CustomizedDialogs_edit>
-        <CustomizedDialogs_delete size='small'>
-          <DeleteUser/>
-        </CustomizedDialogs_delete>
-        </>
->>>>>>> 56aea5c8edd09c1ec7b4b86bb5e664fdc46c6101
       );
     }, }
 ];
@@ -84,14 +65,10 @@ const [tableData, setTableData] = useState([])
         if(response.data.user_info){
           response.data.user_info.forEach(element => {
             if(element.employee_type === '4'){
-<<<<<<< HEAD
               nietos.push({'id':element.id,'uname':element.fname+ ' '+ element.lname,'email':element.email,
-=======
-              nietos.push({'id':element.id,'uname':element.fname + ' ' + element.lname , 'email':element.email, 
->>>>>>> 56aea5c8edd09c1ec7b4b86bb5e664fdc46c6101
               'password':element.password, 'sitename':element.site, 'contact':element.contact, 'address':element.address,
             'empType':element.empType, 'consultant':element.consultant, 'empSec':element.empSec,'empField':element.empField,
-          'empRole':element.empRole,})
+          'empRole':element.empRole, 'empTeam':element.empTeam, 'status':element.status})
             }
           }
             );
