26ff2a87f756b083ca9573cfe411946ebc731f4b
Yasir Fayyaz <yasirfayyaz047@gmail.com>
2023-03-16 17:20:18 +0500

    minor identation in add schedule form

--- src/forms/Add_Schedule/AddSchedule.js
+++ src/forms/Add_Schedule/AddSchedule.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 // import './AddSchedule.css'
 import axios from 'axios';
 import { Grid, TextField, Button, Card, CardContent, MenuItem } from '@mui/material';
@@ -12,7 +12,6 @@ function AddSchedule(props) {
     const [endTime, setEndTime] = useState(null);
     const [addScheduleFormData, setAddScheduleFormData] = useState({ user_selection:'',user_bio_id: '',
                   from_date: null, to_date: null,shift_id:'',leave_status: '',users:usersList});
-    // console.log(props.name)
 
     const formSubmit = (event) => {
         event.preventDefault();
@@ -32,17 +31,14 @@ function AddSchedule(props) {
         console.log(event.target.name);
         const { name, value } = event.target;
         setAddScheduleFormData((preValue) => {
-          console.log([name]);
-          console.log(value);
           console.log(preValue);
-          const updatedValue = {
+          return {
             ...preValue,
             [name]: value
           };
-          // console.log(updatedValue);
-          return updatedValue;
         })
       }
+      console.log(addScheduleFormData);
 
       const UserSelection = (event) => {
         console.log(event.target.value);
@@ -124,101 +120,82 @@ function AddSchedule(props) {
               <CardContent>
                 <form onSubmit={formSubmit}>
                   <Grid>
-                    <Grid xs={12} item>
-                    <TextField label="Select User" name='user_selection' onChange={UserSelection} select value={addScheduleFormData.user_selection} variant="outlined" sx={{ width: "100%" }} required
-                    SelectProps={{
-                      multiple: false
-                    }}>
-                    <MenuItem value="1">1=Single User</MenuItem>
-                    <MenuItem value="2">2=Group by Role</MenuItem>
-                  </TextField>
-                    </Grid>
+                        <Grid xs={12} item>
+                            <TextField label="Choose Option" name='user_selection' onChange={UserSelection} select value={addScheduleFormData.user_selection} variant="outlined" sx={{ width: "100%" }} required
+                            SelectProps={{
+                              multiple: false
+                            }}>
+                            <MenuItem value="1">1=Single User</MenuItem>
+                            <MenuItem value="2">2=Group by Role</MenuItem>
+                          </TextField>
+                        </Grid>
                     {usersList.length > 0 ? (
-                                            <Grid xs={12} item>
-                                            {/* <TextField label="Select User" name='user_selection' onChange={UserSelection} select value={addScheduleFormData.user_selection} variant="outlined" sx={{ width: "100%" }} required
-                                            SelectProps={{
-                                              multiple: false
-                                            }}>
-                                            <MenuItem value="1">1=Single User</MenuItem>
-                                            <MenuItem value="2">2=Group by Role</MenuItem>
-                                          </TextField> */}
-                                          <TextField label="Select shift type" name='user_bio_id' onChange={inputEvent} select value={addScheduleFormData.user_bio_id} variant="outlined" sx={{ width: "100%" }} required
-                                            SelectProps={{
-                                              multiple: false
-                                            }}>
-                                              {
-                                                usersList.map((user,index) => (
-                                                  <MenuItem key={user.id} value={user.id}>
-                                                  {user.name}
-                                                </MenuItem>
-                                                ))
-                                              }
-                                          </TextField>
-                                          </Grid>
-                                          ) : (<div></div>)
+                        <Grid xs={12} item sx={{ mt: 2}}>
+                            <TextField label="Select User" name='user_bio_id' onChange={inputEvent} select value={addScheduleFormData.user_bio_id} variant="outlined" sx={{ width: "100%" }} required
+                              SelectProps={{
+                                multiple: false
+                              }}>
+                                {
+                                  usersList.map((user,index) => (
+                                    <MenuItem key={user.id} value={user.id}>
+                                    {user.name}
+                                  </MenuItem>
+                                  ))
+                                }
+                            </TextField>
+                          </Grid>) : (<div></div>)
+                      }
 
-                                          }
+                      <LocalizationProvider dateAdapter={AdapterDayjs} >
+                      <Stack spacing={2}  sx={{mt:2}}>
+                          <DatePicker
+                                  label="Start Date"
+                                  value={addScheduleFormData.from_date}
+                                  onChange={(handleStartDateChange)}
+                                  variant='outlined'
+                                  sx={{ width: "100%" }}
+                                  required
+                                  renderInput={(params) => <TextField {...params} />}
+                                />
 
-<LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Stack spacing={2}>
-                            <DatePicker
-                                    label="Start Date"
-                                    value={addScheduleFormData.from_date}
-                                    onChange={(handleStartDateChange)}
+                          <DatePicker
+                                    label="End Date"
+                                    value={endTime ?? addScheduleFormData.to_date ?? null}
+                                    onChange={handleEndDateChange}
                                     variant='outlined'
-                                    sx={{ width: "100%" }}
+                                    sx={{ width: "150%"}}
                                     required
                                     renderInput={(params) => <TextField {...params} />}
                                   />
-
-                            <DatePicker
-                                      label="End Date"
-                                      value={endTime ?? addScheduleFormData.to_date ?? null}
-                                      onChange={handleEndDateChange}
-                                      variant='outlined'
-                                      sx={{ width: "150%"}}
-                                      required
-                                      renderInput={(params) => <TextField {...params} />}
-                                    />
-                        </Stack>
-                    </LocalizationProvider>
+                      </Stack>
+                  </LocalizationProvider>
 
                     <Grid xs={12} item sx={{ mt: 2 }}>
                     {
-<TextField
-  label="Shift ID"
-  name="shift_id"
-  select
-  value={addScheduleFormData.shift_id}
-  onChange={(event) => setAddScheduleFormData((prev) => ({ ...prev, shift_id: event.target.value }))}
-  variant="outlined"
-  sx={{ width: "100%" }}
-  required
-  SelectProps={{
-    multiple: false
-  }}
->
-  {
-    props.name.map((shift,index) => (
-      <MenuItem key={shift.id} value={shift.id}>
-      {shift.name}
-    </MenuItem>
-    ))
-  }
-</TextField>
-
+                          <TextField
+                            label="Choose Shift"
+                            name="shift_id"
+                            select
+                            value={addScheduleFormData.shift_id}
+                            onChange={(event) => setAddScheduleFormData((prev) => ({ ...prev, shift_id: event.target.value }))}
+                            variant="outlined"
+                            sx={{ width: "100%" }}
+                            required
+                            SelectProps={{
+                              multiple: false
+                            }}
+                          >
+                            {
+                              props.name.map((shift,index) => (
+                                <MenuItem key={shift.id} value={shift.id}>
+                                {shift.name}
+                              </MenuItem>
+                              ))
+                            }
+                          </TextField>
                         }
                     </Grid>
 
-                    <Grid xs={12} item sx={{ mt: 2 }}>
-                      {/* <TextField label="Select Leave Status" name='leave_status' onChange={inputEvent} select value={addScheduleFormData.leave_status} variant="outlined" sx={{ width: "100%" }} required
-                        SelectProps={{
-                          multiple: false
-                        }}>
-                        <MenuItem value="1">1=Yes</MenuItem>
-                        <MenuItem value="2">2=No</MenuItem>
-                      </TextField> */}
-                    </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                       <Button type="submit" variant="contained" color="primary" sx={{ width: "100%" }} >Add</Button>
                     </Grid>
