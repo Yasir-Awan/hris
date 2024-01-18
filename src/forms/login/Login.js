9f4efec5b8666705612ca5350017271a47a4c2ba
Yasir-Awan <yasirfayyaz047@gmail.com>
2024-01-18 12:18:17 +0500

    url updated

--- src/forms/login/Login.js
+++ src/forms/login/Login.js
@@ -38,7 +38,7 @@ function ModeToggle() {
   );
 }
 
-axios.defaults.baseURL = 'http://58.27.166.43:80/hris/';
+axios.defaults.baseURL = 'http://localhost/hris_ci/';
 
 export default function Login() {
 
